20d739e8b5b277363cdcd09f25dd0e7818707f22
Mohamed Belhedi <mbelhedi@outlook.com>
2022-08-05 21:43:08 +0200

    Das wir eine Fahrschulapp für die praktischen Prüfungen

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View,Alert,ScrollView } from 'react-native';
+import { Card,Button,Title,Paragraph } from 'react-native-paper';
+import { FahrschulCards } from './FahrshuleCards';
+
+
+
 
 export default function App() {
+  
+  useEffect(()=>{
+
+  test()
+
+
+
+
+  })
+  // code ist eine Probe um mit UseState Hook mich anzufreunden obwohl ich Class liebe🤣
+  const[Test,setTest]=useState("Hallo")
+  const [Test1,setTest1]=useState("Hallo2")
+  const [Test3,setTest3]=useState("Hallo3")
+  const test=()=>{
+    setTimeout(()=>{
+     setTest("Hallo")
+      setTest3(Test3) // wird zu einer animation nach 3000 ms 
+
+
+  },3000)}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{Test}</Text>
+      <Text onPress={
+        ()=>setTest("Moin Moin")
+        
+      }> Click mich</Text>
       <StatusBar style="auto" />
+<ScrollView>
+      {FahrschulCards.map(({name,image,title,click})=>(
+        
+        <Card style={styles.cardStyle}>
+        <Card.Title title={name} />
+        <Card.Content>
+          <Title>{title}</Title>
+          <Paragraph>{Test3}</Paragraph>
+        </Card.Content>
+        <Text onPress={()=>setTest3("Was geht")}>Hi</Text>
+        <Card.Cover source={image} />
+        <Card.Actions>
+      
+        </Card.Actions>
+        <Button>Cancel</Button>
+        <Button onPress={click}>Ok</Button>
+      </Card>
+   
+
+
+
+      ))
+        
+    
+      }
+
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +70,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop:40,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cardStyle:{
+    width:500,
+    height:300,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding:20,
+    flex:1
+    
+    
+        }
 });
+
---
+++ FahrshuleCards.js
@@ -0,0 +1,51 @@
+import { Alert } from "react-native"
+// hier kommt das Navigation System mit den Screens
+
+export const FahrschulCards=[
+    {
+        name:"Verkehrszeichen",
+        image:"",
+        title:"Alle Wichtigen Verkehrszeichen",
+        click(){
+
+       Alert.alert()
+
+
+        }
+
+
+
+    },
+    {
+        name:"Vorfahrt",
+        image:"",
+        title:"Wann habe ich Vorfahrt",
+        click(){
+
+
+         
+
+        }
+
+
+
+    },
+    {
+        name:"Rechts Vor Links",
+        image:"",
+        title:"Wie erkenne ich Rechts vor Links",
+        click(){
+
+
+
+        }
+
+
+
+    },
+
+
+
+
+
+]
